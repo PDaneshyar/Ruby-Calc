0d8fe21647a2538f01bf34867859fb3daea08de8
Tech-a28 <tech-a28@Tech-A28s-MacBook-Air.local>
2019-05-07 20:58:48 +0100

    created basic file structure

---
+++ app.rb
@@ -0,0 +1,26 @@
+#  calculates square root
+def square_root(input)
+
+end
+
+# calculates to the power of n
+def power(input1, input2)
+
+end
+
+
+
+  result = 0;
+
+  case operand
+  when +
+    result  = input1 + input2
+  when -
+    result = input1 - input2
+  when *
+    result = input1 * input2
+  when /
+    result = input1 / input2
+  when %
+    result = input1 % input2
+  end
